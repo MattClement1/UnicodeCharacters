78d169efe68b69e49952fbebb4a0abe0a542e6ce
MattClement1 <matthew.clement1@ocsbstudent.ca>
2015-10-16 14:24:33 -0400

    Made unicode program

---
+++ UnicodeCharacters/UnicodeCharacters.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace UnicodeCharacters
+{
+    partial class frmUnicodeCharacters
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstCharacterList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(73, 33);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstCharacterList
+            // 
+            this.lstCharacterList.FormattingEnabled = true;
+            this.lstCharacterList.Location = new System.Drawing.Point(50, 80);
+            this.lstCharacterList.Name = "lstCharacterList";
+            this.lstCharacterList.Size = new System.Drawing.Size(120, 160);
+            this.lstCharacterList.TabIndex = 1;
+            // 
+            // frmUnicodeCharacters
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(220, 270);
+            this.Controls.Add(this.lstCharacterList);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmUnicodeCharacters";
+            this.Text = "Unicode Characters";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstCharacterList;
+    }
+}
+
---
+++ UnicodeCharacters/UnicodeCharacters.cs
@@ -0,0 +1,37 @@
+﻿/*
+*Created by: Matthew Clement
+*Created on: 16-Oct-2015
+*Created for: Unit #3-11
+*This program converts numbers to Unicode
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnicodeCharacters
+{
+    public partial class frmUnicodeCharacters : Form
+    {
+        public frmUnicodeCharacters()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            string letter;
+            for (int number = 65; number <= 90; number++) 
+            {
+                letter = Char.ConvertFromUtf32(number);
+                this.lstCharacterList.Items.Add(letter + " - " + number);
+            }
+        }
+    }
+}
